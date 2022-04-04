cedb9404f3a4179bf5be90f66f0fe207ba2ed7b1
imberlianti <102415170+imberlianti@users.noreply.github.com>
2022-04-04 08:57:37 +0800

    Delete test-print-func.py

--- test-print-func.py
+++
@@ -1,7 +0,0 @@
-def Dompet(JML_MAHASISWA , UANGDOMPET):
-    return JML_MAHASISWA * UANGDOMPET
-
-Jml_Mahasiswa = int(input("Jumlah Mahasiswa: "))
-UangInDompet = int(input("Jumlah Uang: Rp. "))
-
-print("Total Uang: Rp. " + str(Dompet(Jml_Mahasiswa, UangInDompet)))
